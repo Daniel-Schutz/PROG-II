34fc4398838daf8a7fc501c0a52a4139560499b8
RobertoFORTs <bobfortesneto@gmail.com>
2022-10-19 11:16:56 -0300

    Create eP.cpp

---
+++ eP.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+struct acertos{
+    int insc, v_ling, v_mat, v_nat, v_hum;
+    float red;
+};
+struct notas{
+    float EP, notaFinal;
+}; //criei um struct porque parece que vamos precisar guardar essas informações
+
+void eP_NotaFinal(notas relacao[],acertos dadosacertos[], int quant, float media, float desvio){
+
+}
