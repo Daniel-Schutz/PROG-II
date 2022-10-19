3f4810b3d64989803889038c38c9ecfb4eb63934
RobertoFORTs <bobfortesneto@gmail.com>
2022-10-19 11:08:45 -0300

    Update desvioPadrao.cpp

--- desvioPadrao.cpp
+++ desvioPadrao.cpp
@@ -44,4 +44,6 @@ float desvioPadrao(float media, int quant, acertos dadosacertos[], char nomeArea
             somatorio = somatorio + x*x;
         }
     }
+    desvio = sqrt((somatorio/quant-1)); //uso de math.h , acho q é permitido usar
+    return desvio; //lembrar de guardar em variável respectiva à area de conhecimento quando for implementar na main.
 }
