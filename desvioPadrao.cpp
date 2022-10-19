fb912a833a791d27f73b51a9d45f76bfdcfe6d19
RobertoFORTs <bobfortesneto@gmail.com>
2022-10-19 10:51:02 -0300

    Create desvioPadrao.cpp

---
+++ desvioPadrao.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+struct acertos{
+    int insc, v_ling, v_mat, v_nat, v_hum;
+    float red;
+}; //apenas para não dar erro, faremos diferente para disposição de todas as funções
+//nome area sempre deve corresponder aos nomes começados por v_ no struct, para comparação
+int desvioPadrao(float media, acertos dadosacertos[], char nomeArea[7]){ 
+
+}
