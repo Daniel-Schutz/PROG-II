44bdd6a3d1ae59538dd6917cb8877c7f789a2473
RobertoFORTs <bobfortesneto@gmail.com>
2022-10-21 19:21:56 -0300

    Update eP.cpp

--- eP.cpp
+++ eP.cpp
@@ -20,6 +20,6 @@ void eP_NotaFinal(acertos_notas dadosacertos[],tipoCurso pesos[], int quant, flo
         dadosacertos[i].EP[2] = 500 + 100*((2*dadosacertos[i].v_nat - media[2])/desvio[2]);
         dadosacertos[i].EP[3] = 500 + 100*((2*dadosacertos[i].v_hum - media[3])/desvio[3]);
 
-        dadosacertos[i].notaFinal = (dadosacertos[i].red * pesos[i].pesoRed + dadosacertos[i].v_hum * pesos[i].pesoHum + dadosacertos[i].v_ling * pesos[i].pesoLing + dadosacertos[i].v_mat * pesos[i].pesoMat + dadosacertos[i].v_nat * pesos[i].pesoNat)/(pesos[i].pesoRed + pesos[i].pesoHum + pesos[i].pesoLing + pesos[i].pesoMat + pesos[i].pesoNat);
+        dadosacertos[i].notaFinal = (dadosacertos[i].red * pesos[i].pesoRed + dadosacertos[i].EP[3] * pesos[i].pesoHum + dadosacertos[i].EP[0] * pesos[i].pesoLing + dadosacertos[i].EP[1] * pesos[i].pesoMat + dadosacertos[i].EP[2] * pesos[i].pesoNat)/(pesos[i].pesoRed + pesos[i].pesoHum + pesos[i].pesoLing + pesos[i].pesoMat + pesos[i].pesoNat);
     }
 }
