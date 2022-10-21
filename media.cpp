d20efee6d3ac3a17992dba59ff04f3c2dc527cda
RobertoFORTs <bobfortesneto@gmail.com>
2022-10-21 19:59:03 -0300

    Update media.cpp

--- media.cpp
+++ media.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-struct acertos{
+struct acertos_notas{
     int insc, v_ling, v_mat, v_nat, v_hum;
     float red;
+    float EP[4];// EP guardado de acordo com a sequencias das áreas discutidas anteriormente
+    float notaFinal;
 }; //Botei aqui por enquanto so pra ter a base direito e pra não dar erro
 
 //dadosacertos deve ser um vetor
-float mediaDaArea(acertos dadosacertos[], int quant, int soma){ //passar valor correspondente a soma da materia no parametro
+float mediaDaArea(acertos_notas dadosacertos[], int quant, int soma){ //passar valor correspondente a soma da materia no parametro
 
     float media; //variável q será retornada
     media = 2*(soma / quant); // precisa multiplicar por dois para receber o valor correto do EP
