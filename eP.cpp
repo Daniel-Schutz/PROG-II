acdde897c8890d5a5f1786154e45810751d43542
RobertoFORTs <bobfortesneto@gmail.com>
2022-10-21 18:45:26 -0300

    Update eP.cpp

--- eP.cpp
+++ eP.cpp
@@ -10,5 +10,5 @@ struct notas{
 }; //criei um struct porque parece que vamos precisar guardar essas informações
 
 void eP_NotaFinal(notas relacao[],acertos dadosacertos[], int quant, float media, float desvio){
-
+    int i = 0;
 }
