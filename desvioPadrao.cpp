d1827d25583e7ea2daf2244abef8222cba75fa2b
RobertoFORTs <bobfortesneto@gmail.com>
2022-10-22 17:06:41 -0300

    Update desvioPadrao.cpp

--- desvioPadrao.cpp
+++ desvioPadrao.cpp
@@ -19,33 +19,13 @@ float desvioPadrao(float media, int quant, acertos_notas contagem[], char nomeAr
     int i = 0;
 
     //realizar o somatório de (0) até (n-1) e no final calcular o desvio e o retornar.
-    if (!strcmp(nomeArea, "v_ling") == 1){
-        for (i; i < quant; i++){ 
-                //dadosacertos[i] significa acerto individual.
-                //x atualiza para o valor correto em cada chamda do for
-                x = (contagem[i].v_ling - media); //significa a operação básica para o somatório.
-                somatorio = somatorio + x*x;
-            }
-    }
-    else if (!strcmp(nomeArea, "v_mat") == 1){
-        for (i; i < quant; i++){
-            x = (contagem[i].v_mat - media);
+    for (i; i < quant; i++){ 
+            //dadosacertos[i] significa acerto individual.
+            //x atualiza para o valor correto em cada chamda do for
+            x = (contagem[i].nomeArea - media); //significa a operação básica para o somatório.
             somatorio = somatorio + x*x;
         }
-    }
-        
-    else if (!strcmp(nomeArea, "v_nat") == 1){
-        for (i; i < quant; i++){
-            x = (contagem[i].v_nat - media);
-            somatorio = somatorio + x*x;
-        }
-    }
-    else{
-        for (i; i < quant; i++){
-            x = (contagem[i].v_hum - media);
-            somatorio = somatorio + x*x;
-        }
-    }
+
     desvio = sqrt((somatorio/quant-1)); //uso de math.h , acho q é permitido usar
     return 2*desvio; //lembrar de guardar em variável respectiva à area de conhecimento quando for implementar na main.
 }
