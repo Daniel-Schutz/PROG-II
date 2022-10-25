268d0c88e77c62153d8684ef8ea4a4ec5f11ec6a
Daniel-Schutz <109694403+Daniel-Schutz@users.noreply.github.com>
2022-10-24 20:14:17 -0400

    Update funcoesDev.cpp

--- funcoesDev.cpp
+++ funcoesDev.cpp
@@ -3,6 +3,16 @@
 #include "funcoes.h"
 
 // Desenvolver funcoes
-void menu(){
-    // só montando esqueleto nessa parte.
+    void menu(){
+    printf("\n1 - Gerar arquivo de saída .txt");
+    
+    printf("\n2 - Pesquisar candidatos .txt");
+     
+      
+    printf("\n3 - Gerar arquivo dos candidatos não aprovados");
+   
+    printf("\n4 - Alterar nota de redação dos candidatos que entraram com recurso");
+    
+    printf("\n5 - Encerrar programa");
 }
+
