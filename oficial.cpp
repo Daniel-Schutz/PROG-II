b7c6652e8f3ce9fb530d5d303e8ecdea7348af63
Daniel-Schutz <109694403+Daniel-Schutz@users.noreply.github.com>
2022-10-31 23:04:06 -0400

    Update oficial.cpp

--- oficial.cpp
+++ oficial.cpp
@@ -6,14 +6,16 @@
 
 int main(){
 tipoCurso *curso; //tipocurso
-int cont = 0;
-int opcao;
+int opcao;//o~ção que a pessoa irá digitar no menu
 int quant; // quantidade total de cursos
 int qtdAlunos; // quantidade total de candidatos
 float media[4]; // ling / mat / nat / hum
 float desvio[4]; // ling / mat / nat / hum
 int soma[4]; // ling / mat / nat / hum
-char nomePeso[MAX],nomeVaga[MAX],nomeDados[MAX],nomeAcertos[MAX]; //vai precisar mesmo disso ou vai ler direto?
+char nomeArq[MAX]; //nome do arquivo que será lido
+int indiceTipoCurso; //variavel para achar a posição que o codigo está no struct Curso
+int codTemporario;//variavel para salvar temporariamente o codigo do curso
+int qtdTemporaria;//variavel para salvar temporariamente a quantidade de alunos do curso
 
 
 while (opcao!=0){
@@ -24,16 +26,16 @@ while (opcao!=0){
 
 if (opcao==0){
 printf("\nEscreva o nome do arquivo dos cursos e pesos:");
-scanf("%s",nomePeso);
+scanf("%s",nomeArq);
  FILE*  arq;         /* variavel para acessar um arquivo */
 
     /*abertura de um arquivo*/
-    arq = fopen(nomePeso, "r");
+    arq = fopen(nomeArq, "r");
     
     /* verificando se arquivo foi aberto */
     if( arq == NULL )
     {
-        printf("\n\n Arquivo %s nao pode ser aberto.\n\n", nomePeso);
+        printf("\n\n Arquivo %s nao pode ser aberto.\n\n", nomeArq);
     }
     else
     {
@@ -69,23 +71,22 @@ scanf("%s",nomePeso);
 
 //ler e armazenar cursos e vagas
 printf("\nEscreva o nome do arquivo dos cursos e vagas:");
-scanf("%s",nomeVaga);
+scanf("%s",nomeArq);
     
     /*abertura de um arquivo*/
-    arq = fopen(nomeVaga, "r");
+    arq = fopen(nomeArq, "r");
     
     /* verificando se arquivo foi aberto */
     if( arq == NULL )
     {
-        printf("\n\n Arquivo %s nao pode ser aberto.\n\n", nomeVaga);
+        printf("\n\n Arquivo %s nao pode ser aberto.\n\n", nomeArq);
     }
     else
     {
         fscanf(arq, "%d", &quant);
-            int indiceTipoCurso,codVaga;
              for(int i=0;i<quant;i++){
-             fscanf(arq, "%d", &codVaga);  
-             indiceTipoCurso = busca_binariaTipoCurso(quant, curso, codVaga);
+             fscanf(arq, "%d", &codTemporario);  
+             indiceTipoCurso = busca_binariaTipoCurso(quant, curso, codTemporario);
               fscanf(arq, "%d %d %d %d %d %d %d %d %d %d %d", &curso[indiceTipoCurso].AC, &curso[indiceTipoCurso].L1, &curso[indiceTipoCurso].L3, &curso[indiceTipoCurso].L4, &curso[indiceTipoCurso].L5, &curso[indiceTipoCurso].L7,
               &curso[indiceTipoCurso].L8, &curso[indiceTipoCurso].L9, &curso[indiceTipoCurso].L11, &curso[indiceTipoCurso].L13, &curso[indiceTipoCurso].L15); 
              
@@ -104,46 +105,38 @@ scanf("%s",nomeVaga);
    }
 
 
+/*-------------------------------------------------------------ATÉ AQUI TÁ CERTO----------------------------------------------------------------------------------------*/
 
 
-
-
+//VERIFICAR SE TA CERTO PODER REPETIR O COD CURSO< AI VAI TER QUE CRIAR UM STRUCT AUXILIAR PRA SALVAR NESSES CASOS
 //ler e armazenar os dados dos inscritos
 printf("\nEscreva o nome do arquivo dos dados dos inscritos:");
-scanf("%s",nomeDados);
-cursoDados *titulo;
+scanf("%s",nomeArq);
 
     //abertura do arquivo
-    arq = fopen(nomeDados,"r");
+    arq = fopen(nomeArq,"r");
 
     if (arq == NULL){
-        printf("\n\nO arquivo não pode ser aberto\n\n");
+        printf("\n\nO arquivo %s não pode ser aberto\n\n",nomeArq);
     }
 
 
     //executar leitura do arquivo
-    else{     
-        
+    else{    
 
-        titulo = (cursoDados *) malloc(quant * sizeof(cursoDados));
-        if (titulo == NULL){
-            printf("Não foi possível alocar na memória");
-        }
-        else{
+
             for (int i=0;i<quant;i++){ //ler até acabar o arquivo
 
-                fscanf(arq, "%d %d", &titulo[i].codcurso, &titulo[i].qtd); // a primeira linha tem duas entradas
-                //buscar indice no struct principal
-                int indiceTipoCurso_ = busca_binariaTipoCurso(quant, curso, titulo[i].codcurso);
-                //não precisa atribuir o codcurso pois ele já é o mesmo no indiceTipoCurso_
-                curso[indiceTipoCurso_].qtd = titulo[i].qtd; //alocar diretamente no Struct Principal
+                fscanf(arq, " %d %d", &codTemporario, &qtdTemporaria); // a primeira linha tem duas entradas
+            
+                curso[i].qtd = qtdTemporaria; //quantidade de inscritos no curso
 
                 //alocar memória pra cada bloco            
-                curso[indiceTipoCurso_].tuplas = (dadoEmLinhas *) malloc (curso[indiceTipoCurso_].qtd * sizeof(dadoEmLinhas)); //aloca dinâmicamente as linhas diretamente no tipoCurso
+                curso[i].tuplas = (dadoEmLinhas *) malloc (curso[i].qtd * sizeof(dadoEmLinhas)); //aloca dinâmicamente as linhas diretamente no tipoCurso
 
 
                 //verificar se alocou
-                if (curso[indiceTipoCurso_].tuplas == NULL){
+                if (curso[i].tuplas == NULL){
                     printf("Não foi possível alocar na memória");
                     break;
                 }
@@ -151,19 +144,20 @@ cursoDados *titulo;
 
                 else{
                     //executar a leitura dos dados de forma correta para cada bloco;
-                    int x;
-                    for (x=0; x < curso[indiceTipoCurso_].qtd; x++){
-                        fscanf(arq, "%d %[^0^1^2^3^4^5^6^7^8^9] %d/%d/%d %[^\n]", &curso[indiceTipoCurso_].tuplas[x].codinscricao, curso[indiceTipoCurso_].tuplas[x].nomecandidato, &curso[indiceTipoCurso_].tuplas[x].datanasc.dia, &curso[indiceTipoCurso_].tuplas[x].datanasc.mes, &curso[indiceTipoCurso_].tuplas[x].datanasc.ano, curso[indiceTipoCurso_].tuplas[x].tipovaga);
+                    for (int x=0; x < curso[i].qtd; x++){
+                        fscanf(arq, "%d %[^0^1^2^3^4^5^6^7^8^9] %d/%d/%d %[^\n] ", &curso[i].tuplas[x].codinscricao, curso[i].tuplas[x].nomecandidato, &curso[i].tuplas[x].datanasc.dia, &curso[i].tuplas[x].datanasc.mes, &curso[i].tuplas[x].datanasc.ano, curso[i].tuplas[x].tipovaga);
                     }
 
-                
-                    
+                for (int x=0; x < curso[i].qtd; x++){    
+                printf("%d %s %d/%d/%d %s\n",curso[i].tuplas[x].codinscricao, curso[i].tuplas[x].nomecandidato, curso[i].tuplas[x].datanasc.dia, curso[i].tuplas[x].datanasc.mes, curso[i].tuplas[x].datanasc.ano, curso[i].tuplas[x].tipovaga);
+                    }
                 }
-                fclose(arq);
-            }
-            free(titulo);
+                
+            
         }
+        fclose(arq);
     }
+ 
 
 
 
@@ -171,17 +165,17 @@ cursoDados *titulo;
 
 
 printf("\nEscreva o nome do arquivo dos acertos dos inscritos:");
-scanf("%s",nomeAcertos);
+scanf("%s",nomeArq);
 //void lerAcertos(nomeAcertos);//passar mais argumentos
 
     acertos_notas *contagem; //registro a ser alocado dinâmicamente
     
     //
-    arq = fopen(nomeAcertos,"r"); //abrindo arquivo;
+    arq = fopen(nomeArq,"r"); //abrindo arquivo;
     
 
-    if (nomeAcertos==NULL){
-        printf("\n\nO arquivo não pode ser aberto\n\n");
+    if (arq ==NULL){
+        printf("\n\nO arquivo %s não pode ser aberto\n\n",nomeArq);
     }
 
 
@@ -276,8 +270,6 @@ scanf("%s",nomeAcertos);
     
 return 0;
 }  
-
-
 
 
     
