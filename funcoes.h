db6b66d3db4e55f5babd035beae87b105a7bc650
Daniel-Schutz <109694403+Daniel-Schutz@users.noreply.github.com>
2022-11-13 00:36:30 -0400

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -10,7 +10,7 @@ struct data {
 struct tipoAluno {
   char nomecandidato[MAX];
   data datanasc;    // será utilizado para calcular o caso de empate;
-  char tipovaga[5]; // no arquivo em questão aparece com letras e números, como
+  char tipovaga[4]; // no arquivo em questão aparece com letras e números, como
                     // no exemplo dado na DescriçãoTrabalho;
   int codinscricao, v_ling, v_mat, v_nat, v_hum;
   float red;
@@ -25,8 +25,8 @@ struct tipoAluno {
 struct tipoCurso {
   char nomeCurso[MAX]; /* nome do curso + bacharel/licenciatura */
   int codCurso, pesoRed, pesoMat, pesoLing, pesoHum, pesoNat;
-  int AC, L1, L3, L4, L5, L7, L8, L9, L11, L13,
-      L15; // quantidade de vagas para cada tipo
+  int AC, L1, L3, L4, L5, L7, L8, L9, L11, L13, L15; // quantidade de vagas para cada tipo
+   int quantAC, quantL1, quantL3, quantL4, quantL5, quantL7, quantL8, quantL9, quantL11, quantL13, quantL15; 
   int qtd; // quantidade de inscritos por curso;
   tipoAluno *aluno;
 };
@@ -68,16 +68,14 @@ void mergesortVaga(int p, int r, tipoCurso v);
 
 void intercalaVaga(int p, int q, int r, tipoCurso v);
 
-void mergesortNota(int p, int r, tipoCurso v);
+void mergesortNota(int p, int r, tipoAluno v);
 
-void intercalaNota(int p, int q, int r, tipoCurso v);
+void intercalaNota(int p, int q, int r, tipoAluno v);
 
 void naoAprovados(int quant, tipoCurso *curso);
 
 void listaParticipantes(int quant, tipoCurso *curso);
 
-// dadoEmLinhas verificaEmpate(dadoEmLinhas aluno1, tipoAluno
-// notasAluno1,dadoEmLinhas aluno2, tipoAluno notasAluno2);
-
+tipoAluno verificaEmpate(tipoAluno aluno1, tipoAluno aluno2);
 
 
